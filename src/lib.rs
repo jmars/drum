cadda8878a23ffade345ab1fa68d9a2ce4620aff
Jaye Marshall <marshall.jaye@gmail.com>
2016-02-06 18:52:23 +0000

    licensing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 #![feature(plugin, custom_derive, custom_attribute, test, box_syntax)]
 #![plugin(serde_macros)]
 
+// Copyright 2016 drum Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 extern crate bincode;
 extern crate serde;
 
